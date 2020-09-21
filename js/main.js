4de3e8f59a1a88b81951be17b965baefec5f8f6c
ula523 <umaksimova523@gmail.com>
2020-09-21 15:21:29 +0300

    create: added swiper-slider in hotel section

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.slider-button--next',
+    prevEl: '.slider-button--prev',
+  },
+
+  
+})
