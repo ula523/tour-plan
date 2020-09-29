c9ab6bcd62c98de1d2d709f3e30eeee07d721932
ula523 <umaksimova523@gmail.com>
2020-09-29 21:33:54 +0300

    create: mobile menu

--- js/main.js
+++ js/main.js
@@ -24,3 +24,11 @@ var reviewsSlider = new Swiper('.reviews-slider', {
   },
 
 })
+
+var menuButton = document.querySelector(".menu-button");
+menuButton.addEventListener('click', function(){
+  console.log('Клик по кнопке меню');
+  document
+  .querySelector(".navbar-bottom")
+  .classList.toggle('navbar-bottom--visible'); //при клике toggle вешает этот класс или убирает, если он есть
+});
